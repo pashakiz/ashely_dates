9e9f8a6179049760e4d46d165f76decf88dd5933
pashakiz <pashakiz@gmail.com>
2019-04-21 21:26:52 +0300

    fix jquery window load function (for 3 version)

--- js/common.js
+++ js/common.js
@@ -1,13 +1,5 @@
 $(document).ready(function() {
 
-    //preloader
-    setTimeout(function() {
-        var preloader = $('#page-preloader');
-        if ( !preloader.hasClass('done') ) {
-            preloader.addClass('done');
-        }
-    }, 1000);
-
     $('.header-wrap').on('click', '.header__btn', function(){
         $(this).closest('.header-wrap').find('.header-menu').slideToggle();
     });
@@ -16,4 +8,14 @@ $(document).ready(function() {
         $(this).closest('.custom-file').addClass('image');
     });
 
+});
+
+//preloader
+$(window).on('load', function() {
+    setTimeout(function() {
+        var preloader = $('#page-preloader');
+        if ( !preloader.hasClass('done') ) {
+            preloader.addClass('done');
+        }
+    }, 1000);
 });
